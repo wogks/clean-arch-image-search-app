e7ae4bf41e67e66a3400179575bec1eff327e2a6
wogks <wogks27@naver.com>
2022-08-28 00:52:51 +0900

    Feat: api 생성자를 통해서 받아옴

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:image_search_clean/data/api.dart';
 import 'package:image_search_clean/ui/home_screen.dart';
 
 void main() {
@@ -16,7 +17,7 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: const HomeScreen(),
+      home: HomeScreen(api: PixabayApi(),),
     );
   }
 }
