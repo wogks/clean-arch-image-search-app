d3d3f217236a6549534427604fe60b8cb958df77
wogks <wogks27@naver.com>
2022-08-29 00:06:56 +0900

    Feat: view model 생성

--- lib/data/provider.dart
+++ lib/data/provider.dart
@@ -4,13 +4,8 @@ import 'package:image_search_clean/data/api.dart';
 import '../model/photo.dart';
 
 class PhotoProvider extends InheritedWidget {
-  final PixabayApi api;
-  
 
-  final _photoStreamController = StreamController<List<Photo>>()..add([]);
-  Stream<List<Photo>> get photoStream => _photoStreamController.stream;
-
-  PhotoProvider({Key? key, required this.api, required super.child})
+  PhotoProvider({Key? key, required super.child})
       : super(key: key);
 
   static PhotoProvider of(BuildContext context) {
@@ -21,13 +16,8 @@ class PhotoProvider extends InheritedWidget {
     return result!;
   }
 
-  Future<void> fetch(String query) async {
-    final result = await api.fetch(query);
-    _photoStreamController.add(result);
-  }
-
   @override
   bool updateShouldNotify(PhotoProvider oldWidget) {
-    return oldWidget.api != api;
+    return true;
   }
 }
